8cdd89d0fd73cfbab9959b289114c6cf3a2e803b
PJC-1 <jumpzine@gmail.com>
2017-08-27 15:35:57 -0700

    pushing to heroku with database setup and connected

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ HEROKU:
 - A good time to test deploying to heroku, is after setting up your express server.
 - Link to deploying your node.js app to heroku: https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up
 - Remember to modify 'app.listen' to use 'process.env.PORT' which will be set dynamically by Heroku.
+- To set the config var on Heroku execute the following:
+heroku config:set ENVNAME=valueOfEnv
+- To view config vars that are set use: 'heroku config'
+
+MONGOOSE:
+
+DeprecationWarning: open() is deprecated in mongoose >= 4.11.0, use openUri() instead: Workaround is to "npm remove mongoose" then "npm install mongoose@4.10.8 --save", until they fix 4.11.x.
--- app.js
+++ app.js
@@ -1,10 +1,53 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mongo3');
+let db = mongoose.connection;
+
+// Check for DB errors
+db.on('error', function(err){
+    console.log(err);
 });
 
+// Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+// Init App
+const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
+
+// Loud View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
+app.get('/', function(req, res){
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('index', {
+              title : 'Articles',
+              articles: articles
+            });
+        }
+    });
+});
+
+// Add Route
+app.get('/articles/add', function(req, res){
+    res.render('add_article', {
+        title : 'Add Article'
+    });
+});
+
+
+// Start Server
 app.listen(process.env.PORT || 3000, function(){
     console.log('Listening to the smooth sounds of port 3000...');
 });
