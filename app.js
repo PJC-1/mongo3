254b04f578a85ed8fae71af90aab892a30d35284
PJC-1 <jumpzine@gmail.com>
2017-09-03 13:15:12 -0700

    added some validation for the add article

--- app.js
+++ app.js
@@ -111,20 +111,34 @@ app.get('/articles/add', function(req, res){
 
 // Add Submit POST Route
 app.post('/articles/add', function(req, res){
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
+    req.checkBody('title', 'Title is required').notEmpty();
+    req.checkBody('author', 'Author is required').notEmpty();
+    req.checkBody('body', 'Body is required').notEmpty();
 
-    article.save(function(err){
-        if(err){
+    // Get Errors
+    let errors = req.validationErrors();
+
+    if(errors){
+        res.render('add_article', {
+            title:'Add Article',
+            errors:errors
+        })
+    } else {
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+
+        article.save(function(err){
+          if(err){
             console.log(err);
             return;
-        } else {
+          } else {
             req.flash('success', 'Article Added');
             res.redirect('/');
-        }
-    });
+          }
+        });
+    }
 });
 
 // Load Edit Form
